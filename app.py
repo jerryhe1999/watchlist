1e2c14f04964a1df3eec5bf3124e832a0d282ec7
JerryHe <mijerry1999@outlook.com>
2020-12-19 20:55:35 -0800

    Add base template and error template

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def forge():
     db.create_all()
 
     # 全局的两个变量移动到这个函数内
-    name = 'Grey Li'
+    name = 'Jerry He'
     movies = [
         {'title': 'My Neighbor Totora', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
@@ -64,6 +64,11 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
 @app.route('/totoro')
 def totoro():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
@@ -72,9 +77,8 @@ def totoro():
 @app.route('/index')
 @app.route('/home')
 def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -104,3 +108,7 @@ def add_movie(name, title, year):
         user = User.query.first()  # 读取用户记录
         movies = Movie.query.all()  # 读取所有电影记录
         return render_template('index.html', user=user, movies=movies)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
